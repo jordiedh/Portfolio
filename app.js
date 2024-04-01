ac85d3e777859ed718f8637ade20301df06993f9
Jordan Harrison <jordanharrisonttg@gmail.com>
2024-04-01 20:49:36 +0100

    Added URL shortener

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ const __filename = fileURLToPath(
 app.set('view engine', 'ejs'); // Set webpages to run from EJS
 app.use(express.static(path.dirname(__filename) + '/public')); // Set the public view folder to /public (to get images, css, etc.)
 
+
 app.get('/', (req, res) => {
     var projects = [
-        {img: "LightPawn.webp", disabled: true, tags: ["HTML5", "CSS3", "JS", "Express", "SQLite", "Passport Sessions"], title: "Chess Openings", desc: "This site was designed to practice Chess Opening theory, the site has pre-inserted openings, or features to create your own studies.", repo: "https://github.com/jordiedh/ChessOpenings", link: "https://chess.jordanharrisondev.com/"},
-        {img: "1200px-Zendesk_logo.svg.png", disabled: false, tags: ["HTML5", "CSS3", "JS", "Express", "SQLite", "Zendesk API"], title: "Zendesk Dashboard", desc: "For my last job at WorldSmart, I designed a dashboard that hooked up with the Zendesk API to display stats on tickets open in states, tickets closed by individual support agents, etc. This not a live version to avoid displaying this information publicly, but you can see via the source code it hooks up with the API.", repo: "https://github.com/jordiedh/WorldsmartDashboard", link: "https://wsdashboard.jordanharrisondev.com/disclaimer"}
+        {img: "LightPawn.webp", disabled: true, video: false, tags: ["HTML5", "CSS3", "JS", "Express", "SQLite", "Passport Sessions"], title: "Chess Openings", desc: "This site was designed to practice Chess Opening theory, the site has pre-inserted openings, or features to create your own studies.", repo: "https://github.com/jordiedh/ChessOpenings", link: "https://chess.jordanharrisondev.com/"},
+        {img: "1200px-Zendesk_logo.svg.png", disabled: false, video: false, tags: ["HTML5", "CSS3", "JS", "Express", "SQLite", "Zendesk API"], title: "Zendesk Dashboard", desc: "For my last job at WorldSmart, I designed a dashboard that hooked up with the Zendesk API to display stats on tickets open in states, tickets closed by individual support agents, etc. This not a live version to avoid displaying this information publicly, but you can see via the source code it hooks up with the API.", repo: "https://github.com/jordiedh/WorldsmartDashboard", link: "https://wsdashboard.jordanharrisondev.com/disclaimer"},
+        {img: "url.webp", disabled: false, video: true, tags: ["HTML5", "CSS3", "JS", "Express", "SQLite"], title: "URL Shortener", desc: "URL Shortener, easy and simple to use. Turn any URL into a 6 character code. Not hosted, live demo shows a video demonstration.", repo: "https://github.com/jordiedh/URLShortener", link: "https://www.youtube.com/watch?v=tnusGVDQKks"}
     ]
     res.render('home', {
         projects: projects
